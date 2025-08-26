541450e6d684a311d5e8e3930af08158cfca1324
Fernando <gilcorrealuis09@gmail.com>
2025-08-25 21:55:40 -0500

    Actualización de la configuración de la aplicación y mejora de los íconos. Se cambia el nombre de la aplicación a 'IN-NOVA' en el archivo de configuración. Se añaden nuevos íconos de favicon y se actualiza el SVG del favicon para mejorar la presentación visual. Además, se optimiza la configuración de los íconos en la vista principal.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,11 +31,16 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'IN-NOVA') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <!-- Favicon configuration -->
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="theme-color" content="#ffffff">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
